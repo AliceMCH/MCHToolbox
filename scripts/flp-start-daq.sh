2dd4f8d78dca83d820c0ff72a7136305bb3856cd
alice-mch <andrea.ferrero@cern.ch>
2021-08-25 10:30:42 +0200

    Added DAQ script for whole FLP

---
+++ scripts/flp-start-daq.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+#SCRIPT_DIR=$(dirname $0)
+#source ${SCRIPT_DIR}/env.sh
+SCRIPT_DIR=$(pwd)
+
+FLPMAP=flp.map
+
+while read LINE; do
+    
+    CRU=$(echo "$LINE" | cut -f 1)
+    echo "CRU: $CRU"
+
+    if [ -n "$CRU" ]; then
+	./cru-config-trigger.sh $CRU
+    fi
+done < $FLPMAP
+
+#xterm -bg black -fg white -geometry 100x15+0+0 -e watch -n 0.9 ${SCRIPT_DIR}/check-packets-loss.sh  &
+
+rm -f /tmp/readout.done
+rm -f "$HOME/Data/data-flp.raw"
+cat readout-file-full.cfg > readout.cfg
+xterm -bg black -fg white -geometry 100x50+0+200 -e ./start-readout.sh 0 &
+
+sleep 1
+while [ ! -e "$HOME/Data/data-flp.raw" ]; do
+    sleep 1
+done
+
+sleep 2
+
+
+#watch -n 1 ls -lht ${SCRIPT_DIR}/*.raw
+while [ ! -e /tmp/readout.done ]; do
+    sleep 1
+    clear
+    ls -lht $HOME/Data/data-flp.raw | head -n 10
+done
