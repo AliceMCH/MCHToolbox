595968e1cffaf9df37368541ac893f680bf2459b
alice-mch <andrea.ferrero@cern.ch>
2021-11-26 12:50:26 +0100

    Fixed rebuild script

--- tools/command-line/rebuild.sh
+++ tools/command-line/rebuild.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-mkdir -p build && cd build && rm -rf CMake* && cmake3 .. && make -j
+SCRIPTDIR=$(dirname #0)
+
+cd "$SCRIPTDIR" && mkdir -p build && cd build && rm -rf CMake* && cmake3 .. && make -j
