47b8d4f253121d3a4c5ef6517eb9313c68ce1aae
alice-mch <andrea.ferrero@cern.ch>
2022-03-07 09:48:49 +0100

    Added command for setting the UL links enable pattern

--- scripts/cru-config.sh
+++ scripts/cru-config.sh
@@ -63,6 +63,13 @@ if [ x"${CRU_UL_ENABLED}" = "x1" ]; then
     bash ./cru-set-feeid.sh $CRU
 fi
 
+#LINKS_PATT="0x00000000"
+LINKS_PATT="0x0FFF0FFF"
+echo "Setting links pattern for CRU $CRU to ${LINKS_PATT}"
+echo "roc-reg-write --id=${CRU_PCI_ADDR} --channel=2 --address=0xc80008 --value=\"${LINKS_PATT}\""
+roc-reg-write --id=${CRU_PCI_ADDR} --channel=2 --address=0xc80008 --value="${LINKS_PATT}"
+
+
 # set pat-player in default mode
 #echo "roc-pat-player --id=${CRU_PCI_ADDR} --idle=0x0C0300C0300C0300C030 --sync=0x3C0F03C0F03C0F03C0F0 --sync-length=1 --sync-delay=0 --sync-trigger-select=4"
 roc-pat-player --id=${CRU_PCI_ADDR} --idle=0x0C0300C0300C0300C030 \
