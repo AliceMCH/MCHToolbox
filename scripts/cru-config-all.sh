467e423be43be61b1c97936b07e74ecc7fede3b9
alice-mch <andrea.ferrero@cern.ch>
2022-03-07 09:27:21 +0100

    Added 1s delay after CRU configuration

--- scripts/cru-config-all.sh
+++ scripts/cru-config-all.sh
@@ -3,5 +3,5 @@
 CRU=$1
 
 ./cru-init-env.sh $CRU
-./cru-config.sh $CRU && ./cru-solar-config.sh $CRU && sleep 5 && ./cru-ds-config.sh $CRU
+./cru-config.sh $CRU && sleep 1 && ./cru-solar-config.sh $CRU && sleep 5 && ./cru-ds-config.sh $CRU
 ./cru-ul-reset.sh $CRU
