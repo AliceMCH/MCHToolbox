04cced392067fc1e73eefa264d2869f96c62c1fb
alice-mch <andrea.ferrero@cern.ch>
2022-03-23 15:16:10 +0100

    Improved GBT FEC test

--- scripts/cru-fec-test.sh
+++ scripts/cru-fec-test.sh
@@ -4,6 +4,18 @@ CRU=$1
 SCRIPTDIR=$(readlink -f $(dirname $0))
 source ${SCRIPTDIR}/env-${CRU}.sh
 
+while [ true ]; do
+    bash ./cru-check-link-status.sh $CRU
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    echo ""; echo "";
+    RED='\033[0;31m'
+    NC='\033[0m' # No Color
+    echo -n -e "${RED}Some SOLAR boards are DOWN, please try to power-cycle the corresponding crates and press enter...${NC}"
+    read dummy
+done
+
 GBTCMD=../tools/command-line/build/gbt-config
 
 NLINKS=$(echo "${CRU_LINKS1}" | tr "," "\n" | wc -l)
@@ -30,7 +42,13 @@ done
 
 echo "${FECL1}"
 
-sleep 1
+TIMEOUT=600
+while [ $TIMEOUT -gt 0 ]; do
+    sleep 10
+    TIMEOUT=$((TIMEOUT-10))
+    echo -n "...${TIMEOUT} "
+done
+echo ""
 
 for L in $(seq 1 $NLINKS); do
     LINKID=$(echo "${CRU_LINKS1}" | tr "," "\n" | sed -n ${L}p)
@@ -47,13 +65,19 @@ FEC_OK=1
 for L in $(seq 1 $NLINKS); do
 
     LINKID=$(echo "${CRU_LINKS1}" | tr "," "\n" | sed -n ${L}p)
+    DP=$((LINKID/12))
+    LINKID2=${LINKID}
+    if [ $DP -gt 0 ]; then
+	LINKID2=$((LINKID-12))
+    fi
+    
     FEC1=$(echo "${FECL1}" | cut -d' ' -f $((L+2)))
     FEC2=$(echo "${FECL2}" | cut -d' ' -f $((L+2)))
     FEC_OK=1
     if [ "x${FEC2}" != "x${FEC1}" ]; then
 	RED='\033[0;31m'
 	NC='\033[0m' # No Color
-	echo -e "${RED}Link $LINKID FEC counter mismatch${NC}"
+	echo -e "${RED}Link $LINKID ($DP/$LINKID2) FEC counter mismatch${NC}"
         echo "$FEC1 $FEC2"
  	FEC_OK=0
 	#echo "link $LINKID FEC_OK=${FEC_OK}"
