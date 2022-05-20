60b3f1b0474d821dd0cea610700f28287cb168da
alice-mch <andrea.ferrero@cern.ch>
2022-05-20 08:23:41 +0200

    Fixed PCI rescan script

--- scripts/pci-rescan.sh
+++ scripts/pci-rescan.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-for i in 3b 3c af b0 d8 d9 ; do  echo 1 > /sys/bus/pci/devices/0000\:$i\:00.0/remove; echo 1 > /sys/bus/pci/rescan; done
+for i in 3b 3c af b0 d8 d9 ; do
+	echo 1 > /sys/bus/pci/devices/0000\:$i\:00.0/remove
+done
+echo 1 > /sys/bus/pci/rescan
