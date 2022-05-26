fae7f162acc5caecde5ebfd3e03a183430095e84
alice-mch <andrea.ferrero@cern.ch>
2022-05-26 22:04:05 +0200

    Added back SVL reset at link initialization

--- tools/command-line/src/Sca/hdlc_alf.hpp
+++ tools/command-line/src/Sca/hdlc_alf.hpp
@@ -59,7 +59,7 @@ class HdlcAlf : public common::HdlcCore
      */
     void rst() final override
     {
-      //sendSvlReset();
+      sendSvlReset();
       sca.scReset();
       //std::this_thread::sleep_for(std::chrono::milliseconds(100));
       //sendSvlConnect();
