d1e39c3420bec274ec0bae1ed0ac6536ad949fe5
alice-mch <andrea.ferrero@cern.ch>
2022-06-13 13:43:33 +0200

    Added script for saving the GBT clock phases for the complete FLP

---
+++ scripts/flp-solar-clock-phase-save.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+FLPMAP=flp.map
+
+HOLD="-hold"
+if [ x"$1" = "x-e" ]; then
+    HOLD=""
+fi
+
+while read LINE; do
+
+    CRU=$(echo "$LINE" | cut -f 1)
+
+    ./cru-solar-clock-phase-save.sh $CRU
+    
+#    if [ -n "$CRU" ]; then
+#	xterm -bg black -fg white -geometry 100x45 -T "cru-solar-clock-phase-save.sh $CRU" $HOLD -e ./cru-solar-clock-phase-save.sh $CRU &
+#    fi
+
+done < "$FLPMAP"
+
+# wait for all CRUs to complete
+#wait
