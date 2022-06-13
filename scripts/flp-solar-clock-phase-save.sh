86da31105220b78f92701259653deca99c9b0f68
alice-mch <andrea.ferrero@cern.ch>
2022-06-13 17:00:05 +0200

    Read GBT clock phases in parallel

--- scripts/flp-solar-clock-phase-save.sh
+++ scripts/flp-solar-clock-phase-save.sh
@@ -11,13 +11,13 @@ while read LINE; do
 
     CRU=$(echo "$LINE" | cut -f 1)
 
-    ./cru-solar-clock-phase-save.sh $CRU
+#    ./cru-solar-clock-phase-save.sh $CRU
     
-#    if [ -n "$CRU" ]; then
-#	xterm -bg black -fg white -geometry 100x45 -T "cru-solar-clock-phase-save.sh $CRU" $HOLD -e ./cru-solar-clock-phase-save.sh $CRU &
-#    fi
+    if [ -n "$CRU" ]; then
+	xterm -bg black -fg white -geometry 100x45 -T "cru-solar-clock-phase-save.sh $CRU" $HOLD -e ./cru-solar-clock-phase-save.sh $CRU &
+    fi
 
 done < "$FLPMAP"
 
 # wait for all CRUs to complete
-#wait
+wait
