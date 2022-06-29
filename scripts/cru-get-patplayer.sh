ccec1016a91489a7066218a90f992d7cbcc58923
alice-mch <andrea.ferrero@cern.ch>
2022-06-29 16:11:25 +0200

    Added script to read back patplayer configuration

---
+++ scripts/cru-get-patplayer.sh
@@ -0,0 +1,86 @@
+#! /bin/bash
+
+FORCE_CONFIG=""
+if [ x"$1" = "x-f" ]; then FORCE_CONFIG="--force-config"; shift; fi
+
+CRU=$1
+
+source env-${CRU}.sh
+
+P1=$(roc-reg-read --id=${CRU_PCI_ADDR} --ch=2 --add=0x00260010 | cut -d"x" -f 2)
+P2=$(roc-reg-read --id=${CRU_PCI_ADDR} --ch=2 --add=0x00260014 | cut -d"x" -f 2)
+P3=$(roc-reg-read --id=${CRU_PCI_ADDR} --ch=2 --add=0x00260018 | cut -d"x" -f 2)
+
+while [ true ]; do
+    L1=$(echo ${P1} | wc -c)
+    L2=$(echo ${P2} | wc -c)
+    L3=$(echo ${P3} | wc -c)
+
+    if [ $L1 -le 8 ]; then
+	P1="0${P1}"
+    fi
+    if [ $L2 -le 8 ]; then
+	P2="0${P2}"
+    fi
+    if [ $L3 -le 8 ]; then
+	P3="0${P3}"
+    fi
+
+    if [ $L1 -gt 8 -a $L2 -gt 8 -a $L3 -gt 8 ]; then
+	break
+    fi
+done
+
+echo "SYNC: 0x${P3}${P2}${P1}"
+
+P1=$(roc-reg-read --id=${CRU_PCI_ADDR} --ch=2 --add=0x0026001C | cut -d"x" -f 2)
+P2=$(roc-reg-read --id=${CRU_PCI_ADDR} --ch=2 --add=0x00260020 | cut -d"x" -f 2)
+P3=$(roc-reg-read --id=${CRU_PCI_ADDR} --ch=2 --add=0x00260024 | cut -d"x" -f 2)
+
+while [ true ]; do
+    L1=$(echo ${P1} | wc -c)
+    L2=$(echo ${P2} | wc -c)
+    L3=$(echo ${P3} | wc -c)
+
+    if [ $L1 -le 8 ]; then
+	P1="0${P1}"
+    fi
+    if [ $L2 -le 8 ]; then
+	P2="0${P2}"
+    fi
+    if [ $L3 -le 8 ]; then
+	P3="0${P3}"
+    fi
+
+    if [ $L1 -gt 8 -a $L2 -gt 8 -a $L3 -gt 8 ]; then
+	break
+    fi
+done
+
+echo "RST:  0x${P3}${P2}${P1}"
+
+P1=$(roc-reg-read --id=${CRU_PCI_ADDR} --ch=2 --add=0x00260004 | cut -d"x" -f 2)
+P2=$(roc-reg-read --id=${CRU_PCI_ADDR} --ch=2 --add=0x00260008 | cut -d"x" -f 2)
+P3=$(roc-reg-read --id=${CRU_PCI_ADDR} --ch=2 --add=0x0026000C | cut -d"x" -f 2)
+
+while [ true ]; do
+    L1=$(echo ${P1} | wc -c)
+    L2=$(echo ${P2} | wc -c)
+    L3=$(echo ${P3} | wc -c)
+
+    if [ $L1 -le 8 ]; then
+	P1="0${P1}"
+    fi
+    if [ $L2 -le 8 ]; then
+	P2="0${P2}"
+    fi
+    if [ $L3 -le 8 ]; then
+	P3="0${P3}"
+    fi
+
+    if [ $L1 -gt 8 -a $L2 -gt 8 -a $L3 -gt 8 ]; then
+	break
+    fi
+done
+
+echo "IDLE: 0x${P3}${P2}${P1}"
