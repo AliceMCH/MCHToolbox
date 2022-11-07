a07583f542ca27095d35d9a25351cee0dbb00d95
alice-mch <andrea.ferrero@cern.ch>
2022-11-07 15:20:23 +0100

    Fixed script to turn off clocks on all links

--- scripts/cru-gbt-off.sh
+++ scripts/cru-gbt-off.sh
@@ -4,7 +4,7 @@ CRU=$1
 
 source env-${CRU}.sh
 
-FORCE_CONFIG="--force-config --bypass-fw-check --clock=${CRU_CLKSRC} --datapathmode=STREAMING --gbtmux=ttc --downstreamdata=pattern"
+FORCE_CONFIG="--force-config --bypass-fw-check --links 0-11 --clock local --datapathmode packet --gbtmux ttc"
 
 echo "roc-config ${FORCE_CONFIG} --id=${CRU_SN}:0 --loopback"
 roc-config ${FORCE_CONFIG} --id=${CRU_SN}:0 --loopback
