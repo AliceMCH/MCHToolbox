d139ab6bebfdba19c4c06caec2a6ea6146a54a5b
alice-mch <andrea.ferrero@cern.ch>
2023-11-29 14:28:32 +0100

    Updated hard reset command

--- scripts/cru-send-hard-reset.sh
+++ scripts/cru-send-hard-reset.sh
@@ -5,4 +5,27 @@ CRU=$1
 SCRIPTDIR=$(dirname $0)
 source ${SCRIPTDIR}/env-${CRU}.sh
 
-roc-pat-player --id=${CRU_PCI_ADDR} --idle=${PATT_IDLE} --reset 0x0 --reset-length=3000  --trigger-reset
+PATT1=${PATT_IDLE}
+P0=$(echo -n ${PATT1} | cut -c 15-22)
+P1=$(echo -n ${PATT1} | cut -c 7-14)
+P2=$(echo -n ${PATT1} | cut -c 3-6)
+
+for addr in ${CRU_PCI_ADDR1} ${CRU_PCI_ADDR2}; do
+
+    ${prefix}o2-roc-reg-write --id=${addr} --ch=2 --address=00260004 --val=0x${P0}
+    ${prefix}o2-roc-reg-write --id=${addr} --ch=2 --address=00260008 --val=0x${P1}
+    ${prefix}o2-roc-reg-write --id=${addr} --ch=2 --address=0026000C --val=0x${P2}
+
+    ${prefix}o2-roc-reg-write --id=${addr} --ch=2 --address=0026001C --val=0x0
+    ${prefix}o2-roc-reg-write --id=${addr} --ch=2 --address=00260020 --val=0x0
+    ${prefix}o2-roc-reg-write --id=${addr} --ch=2 --address=00260024 --val=0x0
+
+    # RESET LENGTH
+    ${prefix}o2-roc-reg-write --id=${addr} --ch=2 --address=00260030 --val=0xBB8
+
+    # TRIGGER RESET
+    ${prefix}o2-roc-reg-write --id=${addr} --ch=2 --address=00260000 --val=0x10
+
+done
+
+#roc-pat-player --id=${CRU_PCI_ADDR} --idle=${PATT_IDLE} --reset 0x0 --reset-length=3000  --trigger-reset
